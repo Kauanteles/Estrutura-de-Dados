2341d31eeef372060d47012ea02a5cf31b58b0b3
Kauan Souza Teles <116807100+Kauanteles@users.noreply.github.com>
2023-01-06 01:54:15 -0300

    Delete Projeto 3 versão directory

--- Projeto 3 versão/Main.java
+++
@@ -1,9 +0,0 @@
-class Main {
-  public static void main(String[] args) {
-    Cachorro dog1 = new Cachorro(1,"tchonga", false, "pug");
-    Fila <Cachorro> fila = new Fila<Cachorro>(dog1);
-
-      fila.enfileira(dog1);
-      System.out.println(fila.toString());
-  }
-}
